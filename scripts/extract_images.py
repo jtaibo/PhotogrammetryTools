7279070e40ed2e952d336fd8c0e6a8416f869857
Javier Taibo <javier.taibo@gmail.com>
2025-11-23 17:45:29 +0100

    Work in progress...

--- scripts/extract_images.py
+++ scripts/extract_images.py
@@ -2,9 +2,14 @@ import glob
 import os
 import cv2
 import argparse
-#import json
-#import statistics
+import json
+import csv
+import statistics
 import numpy as np
+import datetime
+import sys
+from timeit import default_timer as timer
+import exiftool
 
 
 # NOTE: In this example, "U:/ffmpeg/bin" path contains ffmpeg and exiftool executables
@@ -15,10 +20,24 @@ parser.add_argument("input_video", help="Path for the video to split into frames
 parser.add_argument("--exif_ref", help="Path to the image with the EXIF metadata to populate the frames extracted from the video")
 parser.add_argument("--folder", help="Path to folder where to place the extracted frames")
 parser.add_argument("--framePrefix", help="Frame file name prefix")
+parser.add_argument("--frameScale", help="Scaling factor for extracted frames", default=1)
 
-parser.add_argument("--sharpness_threshold", default = 55)
-parser.add_argument("--diff_threshold", default = 0)
-parser.add_argument("--flow_threshold", default = 30)
+# TO-DO
+# Keep all frames (raw, filtered)
+parser.add_argument("--keep_all", help="Keep all frames extracted from video or filtered ones only", default = False)
+
+# Step selection (extract, filter, exif)
+parser.add_argument("--no_extract", help="Skip frames extraction (frames are already extracted)", action=argparse.BooleanOptionalAction, default = False)
+parser.add_argument("--no_filter", help="Extract frames only, do not filter them", action=argparse.BooleanOptionalAction, default = False)
+# EXIF are already skipped when no reference file is given
+
+# EXIF from file or from parameters (make, model, sensor_width)
+
+parser.add_argument("--report_stats", help="Report statistics (sharpness)", action=argparse.BooleanOptionalAction, default = False)
+
+parser.add_argument("--sharpness_threshold", type=float, default = 55)
+parser.add_argument("--diff_threshold", type=float, default = 0)
+parser.add_argument("--flow_threshold", type=float, default = 30)
 
 args = parser.parse_args()
 
@@ -50,7 +69,7 @@ def copyEXIFFromVideo(input_file, output_name, frames_folder):
 #
 #   TO-DO: expose parameters (start time, duration, fps, resolution, ...)
 ###############################################################################
-def extractFrames(input_file, output_name, frames_folder):
+def extractFrames(input_file, output_name, frames_folder, scale):
    
     # Create a new directory to avoid messing up too much
     if os.path.isdir(frames_folder):
@@ -68,7 +87,9 @@ def extractFrames(input_file, output_name, frames_folder):
     FRAME_STEP=vinfo["fps"]
     EXTENSION="jpg"
 
-    cmd = "ffmpeg -ss " + str(START_TIME) + " -t " + str(DURATION) + " -i \"" + input_file + "\" -r " + str(FRAME_STEP) + " " + frames_folder + "/" + output_name + "%04d." + EXTENSION
+    SCALE=scale
+
+    cmd = "ffmpeg -ss " + str(START_TIME) + " -t " + str(DURATION) + " -i \"" + input_file + "\" -r " + str(FRAME_STEP) + " -vf scale=\"iw*" + str(SCALE) + ":ih*" + str(SCALE) +"\" " + frames_folder + "/" + output_name + "%04d." + EXTENSION
     os.system(cmd)
     #print(cmd)
 
@@ -108,12 +129,31 @@ def addExifInfo(frames_list, exif_ref):
     exiftool_bin = "exiftool.exe"
     metadata_source = exif_ref
 
-    for filepath in frames_list:
-        print("Adding EXIF metadata to " + filepath)
-        cmd = exiftool_bin + " -overwrite_original -TagsFromFile \"" + metadata_source + "\" \"" + filepath + "\""
+#    for filepath in frames_list:
+#        print("Adding EXIF metadata to " + filepath)
+#        cmd = exiftool_bin + " -overwrite_original -TagsFromFile \"" + metadata_source + "\" \"" + filepath + "\""
         #print(cmd)
-        os.system(cmd)
+#        os.system(cmd)
         #print(filepath)
+
+
+    with exiftool.ExifToolHelper() as et:
+
+        tags = et.get_tags( exif_ref, tags = ["make", "model", "focalLength"])
+
+        make = tags[0]["EXIF:Make"]
+        model = tags[0]["EXIF:Model"]
+        focal_length = tags[0]["EXIF:FocalLength"]
+        #serial_number = tags[0]["EXIF:SerialNumber"]
+
+        et.set_tags( frames_list,
+                    tags={"make": make,
+                          "model": model,
+                          "focalLength": focal_length}
+                          #"serialNumber": serial_number}
+                    params=["-overwrite_original"]
+        )
+
 
 ###############################################################################
 # 
@@ -154,7 +194,7 @@ def getSharpness(frames_list):
 ###############################################################################
 # 
 ###############################################################################
-def filterImages(frames_list):
+def filterImages(frames_list, sharpness_threshold, diff_threshold, flow_threshold):
     # The filtering thresholds can come from CLI arguments or from statistics
     # computed among all extracted frames to keep the better ones
     # We could also limit the number of frames to use so the script can select
@@ -163,15 +203,15 @@ def filterImages(frames_list):
     for filepath in frames_list:
         score = computeSharpness(filepath)
         #print(filepath + " -> " + str(score))
-        if score < args.sharpness_threshold:
-            print("Sharpness level", score, "lower than threshold", args.sharpness_threshold, "Discarding frame", filepath)
+        if score < sharpness_threshold:
+            print("Sharpness level", score, "lower than threshold", sharpness_threshold, "Discarding frame", filepath)
             os.remove(filepath)
         else:
             # Check difference with previous frame
             if previous:
                 diff, flow = computeDifference(previous, filepath)
                 #print(previous, "->", filepath, ":", diff, flow)
-                if diff < args.diff_threshold or flow < args.flow_threshold:
+                if diff < diff_threshold or flow < flow_threshold:
                     print("Low difference. Removing frame ", filepath)
                     os.remove(filepath)
                 else:
@@ -185,8 +225,12 @@ def filterImages(frames_list):
 ###############################################################################
 def main():
 
+    print("Starting frame extraction and processing - ", datetime.datetime.now().isoformat())
+    print("COMMAND LINE:", sys.argv)
+    start_time = timer()
+
     if not args.folder:
-        frames_folder = os.path.basename(args.input_file)
+        frames_folder = os.path.basename(args.input_video)
     else:
         frames_folder = args.folder
 
@@ -195,23 +239,45 @@ def main():
     else:
         frame_prefix = args.framePrefix
 
-    extractFrames(args.input_video, frame_prefix, frames_folder)
+    if args.no_extract:
+        print("Skipping extraction stage")
+    else:
+        print("Extracting frames...")
+        extractFrames(args.input_video, frame_prefix, frames_folder, args.frameScale)
     #removeBlurredImages(getFramesInFolder(frames_folder))
     #removeSimilarImages(getFramesInFolder(frames_folder))
 
-    # sharpness = getSharpness(getFramesInFolder(frames_folder))
-    # sh_mean = statistics.mean(sharpness.values())
-    # sh_min = min(sharpness.values())
-    # sh_max = max(sharpness.values())
-    # sh_stdev = statistics.stdev(sharpness.values())
-    # print("MEAN:", sh_mean, "MIN:", sh_min, "MAX:", sh_max, "STDEV:", sh_stdev)
-    # with open('sharpness.json', 'w') as fp:
-    #     json.dump(sharpness, fp)
+    if args.report_stats:
+        stats_filename = frame_prefix + 'stats'
+        print("Dumping stats to", stats_filename)
+        sharpness = getSharpness(getFramesInFolder(frames_folder))
+        sh_mean = statistics.mean(sharpness.values())
+        sh_min = min(sharpness.values())
+        sh_max = max(sharpness.values())
+        sh_stdev = statistics.stdev(sharpness.values())
+        #print("MEAN:", sh_mean, "MIN:", sh_min, "MAX:", sh_max, "STDEV:", sh_stdev)
+        # JSON
+        with open(stats_filename + ".json", 'w') as fp:
+            json.dump(sharpness, fp)
+        # CSV
+        with open(stats_filename + ".csv", 'w', newline='') as fp:
+            csvwriter = csv.writer(fp, delimiter=',',
+                                   quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            csvwriter.writerows(sharpness.items())
 
     # Filter images (considering blurriness and difference with previous image)
-    filterImages(getFramesInFolder(frames_folder))
+    if args.no_filter:
+        print("Skipping filtering stage")
+    else:
+        print("Filtering images...")
+        filterImages(getFramesInFolder(frames_folder), args.sharpness_threshold, args.diff_threshold, args.flow_threshold)
 
     if args.exif_ref:
+        print("Adding EXIF metadata...")
         addExifInfo(getFramesInFolder(frames_folder), args.exif_ref)
+
+    print("Finished frame extraction and processing - ", datetime.datetime.now().isoformat())
+    end_time = timer()
+    print("Elapsed time:", end_time-start_time, "seconds")
 
 main()
---
+++ scripts/install_dependencies.bat
@@ -0,0 +1,8 @@
+@echo off
+
+python.exe -m pip install --upgrade pip
+
+python.exe -m pip install opencv-python
+python -m pip install -U pyexiftool
+
+pause
